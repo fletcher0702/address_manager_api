a739413ae0512020847d83416728b7ad00c2715e
ABEDIER <abediernelson@live.fr>
2019-04-05 19:39:13 +0200

    core: little maintenance

--- src/main/java/com/fantech/addressmanager/api/dto/team/UninviteUserDto.java
+++ src/main/java/com/fantech/addressmanager/api/dto/team/UninviteUserDto.java
@@ -1,4 +1,20 @@
 package com.fantech.addressmanager.api.dto.team;
 
 public class UninviteUserDto {
+
+    private String userUuid;
+    private String teamUuid;
+    private String email;
+
+    public String getUserUuid() {
+        return userUuid;
+    }
+
+    public String getTeamUuid() {
+        return teamUuid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
